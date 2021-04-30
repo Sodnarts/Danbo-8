8feb39bccde76a1be59018c27a39d6e9cd9dad93
Glenn Robert Strandos <strandos.glenn@gmail.com>
2021-04-30 13:35:01 +0200

    add clock part

---
+++ src/common/clock-module/clock.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./clock.css";
+
+const Clock = () => {
+    return (
+      <div className="container">
+        <div className="clock">YO</div>
+      </div>
+    );
+}
+  
+export { Clock };
+  
