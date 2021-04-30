86e71ec98a9b04128043eb0de1cbda495cecd43f
felisep <felixsebae@gmail.com>
2021-04-30 13:55:46 +0200

    Changed music component

--- src/common/song/song-module.js
+++ src/common/song/song-module.js
@@ -1,36 +1,35 @@
+import React, { Component } from 'react'
 
-class PlaySound{
+class Music extends Component {
   constructor(props) {
-    super(props);
-
-    this.state = {
-      play: true
-    };
-
-    this.filePath = "public/El Chombo - Chacarron.mp3";
-    this.audio = new Audio(this.filePath);
-    this.audio.addEventListener('ended', function () {
-      this.currentTime = 0;
-      this.play();
-    }, false);
-    this.togglePlay = this.togglePlay.bind(this);
+  super(props);
+  this.state = {
+    play: false,
+    pause: true,
   }
-  togglePlay() {
-    this.setState({
-        play: !this.state.play
-    });
+  this.relativePath = "public/El Chombo - Chacarron.mp3";
+  this.audio = new Audio(this.relativePath);
+}
 
-    this.state.play ? this.audio.play() : this.audio.pause();
+play = () => {
+this.setState({ play: true, pause: false })
+  this.audio.play();
+}
+
+pause = () => {
+this.setState({ play: false, pause: true })
+  this.audio.pause();
 }
 
 render() {
-    return (
-        <div>
-            <button
-                id="audioBtn"
-                onClick={this.togglePlay}> {this.state.play ? <PlayArrow /> : <Pause />}
-            </button>
-        </div>
-    );
-  }
+  
+return (
+  <div>
+    <button onClick={this.play}>Play</button>
+    <button onClick={this.pause}>Pause</button>
+  </div>
+  );
 }
+}
+
+export{ Music };
