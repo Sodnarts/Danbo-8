2e0fc3489f61157a865471b6ade2a74f1309290d
Glenn Robert Strandos <strandos.glenn@gmail.com>
2021-04-30 14:37:03 +0200

    Update clock

--- src/common/clock-module/clock.js
+++ src/common/clock-module/clock.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./clock.css";
-import {ExpandLess} from '@material-ui/icons';
+import {ExpandLess, ExpandMore} from '@material-ui/icons';
 
 class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       timer: {
-        seconds: 10,
+        seconds: 0,
         minutes: 0,
         hours: 0,
       },
@@ -24,7 +24,7 @@ class Clock extends React.Component {
         if (minutes > 0) {
           this.setState({timer: {seconds: 59, hours: hours, minutes: minutes - 1}})
         } else {
-          if (hours > 0 ) {
+          if (hours > 0) {
             this.setState({timer: {seconds: 59, hours: hours - 1, minutes: 59}})
           } else {
             this.setState({isCountingDown: false}, this.callAlarm())
@@ -47,6 +47,40 @@ class Clock extends React.Component {
   callAlarm = () => {
     console.log("CALLING ALARM")
   }
+
+  increaseTimer = () => {
+    const { seconds, minutes, hours }= this.state.timer
+      if (seconds < 59) {
+        this.setState({timer: {seconds: seconds + 1, hours: hours, minutes: minutes}})
+      } else {
+        if (minutes < 59) {
+          this.setState({timer: {seconds: 0, hours: hours, minutes: minutes + 1}})
+        } else {
+          if (hours < 99) {
+            this.setState({timer: {seconds: 0, hours: hours + 1, minutes: 0}})
+          } else {
+            console.log("max hours");
+          }
+        }
+      }
+  }
+
+  decreaseTimer = () => {
+    const { seconds, minutes, hours }= this.state.timer
+      if (seconds > 0) {
+        this.setState({timer: {seconds: seconds - 1, hours: hours, minutes: minutes}})
+      } else {
+        if (minutes > 0) {
+          this.setState({timer: {seconds: 59, hours: hours, minutes: minutes - 1}})
+        } else {
+          if (hours > 0) {
+            this.setState({timer: {seconds: 59, hours: hours - 1, minutes: 59}})
+          } else {
+            console.log("Cannot be lower");
+          }
+        }
+      }
+  }
   
   render() {
     const {seconds, minutes, hours} = this.state.timer;
@@ -54,8 +88,12 @@ class Clock extends React.Component {
 
     return (
       <div className="container">
-        <ExpandLess></ExpandLess>
-        <div className="clock">{hours.toString().length > 1 ? hours : "0" + hours}:{minutes.toString().length > 1 ? minutes : "0" + minutes}:{seconds.toString().length > 1 ? seconds : "0" + seconds}</div>
+        
+        <div className="clock">
+          <a onClick={this.increaseTimer}><ExpandLess style={{display: 'inline-block', marginRight: '8px'}}/></a>
+          {hours.toString().length > 1 ? hours : "0" + hours}:{minutes.toString().length > 1 ? minutes : "0" + minutes}:{seconds.toString().length > 1 ? seconds : "0" + seconds}
+          <a onClick={this.decreaseTimer}><ExpandMore style={{display: 'inline-block', marginLeft: '8px'}}/></a>
+        </div>
         <button className="button" onClick={this.handleCoundown}>{isCountingDown ? 'Stop countdown' : 'Start countdown'}</button>
       </div>
     );
