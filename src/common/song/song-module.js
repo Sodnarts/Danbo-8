743b7d9da5b21fc3b6b08f2874352c085859d913
felisep <felixsebae@gmail.com>
2021-04-30 14:09:23 +0200

    Removed logos and changed filepath for mp3

--- src/common/song/song-module.js
+++ src/common/song/song-module.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Music extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    play: false,
-    pause: true,
-  }
-  this.relativePath = "public/El Chombo - Chacarron.mp3";
-  this.audio = new Audio(this.relativePath);
+    constructor(props) {
+        super(props);
+        this.state = {
+            play: false,
+            pause: true,
+        };
+        this.relativePath = '/Users/felipesepulveda/git/Danbo-8/public/El Chombo - Chacarron.mp3';
+        this.audio = new Audio(this.relativePath);
+    }
+
+    play = () => {
+        this.setState({ play: true, pause: false });
+        this.audio.play();
+    };
+
+    pause = () => {
+        this.setState({ play: false, pause: true });
+        this.audio.pause();
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.play}>Play</button>
+                <button onClick={this.pause}>Pause</button>
+            </div>
+        );
+    }
 }
 
-play = () => {
-this.setState({ play: true, pause: false })
-  this.audio.play();
-}
-
-pause = () => {
-this.setState({ play: false, pause: true })
-  this.audio.pause();
-}
-
-render() {
-  
-return (
-  <div>
-    <button onClick={this.play}>Play</button>
-    <button onClick={this.pause}>Pause</button>
-  </div>
-  );
-}
-}
-
-export{ Music };
+export { Music };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import { Music } from './common/song/song-module';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Music />
   </React.StrictMode>,
   document.getElementById('root')
 );
